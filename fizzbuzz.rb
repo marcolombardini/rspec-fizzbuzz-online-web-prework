dbfcadd0760719dc116b76b93cd8d551af024819
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-04 21:36:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if  int % 3 == 0 
-   return "Fizz"
+   return "FizzBuzz"
  elsif int % 5 == 0 
    return "Buzz"
  elsif int (% 3 == 0) == (% 5 == 0)
